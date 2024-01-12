4936a8ba82288da1693510ff142f78f7bfe74c20
Bradleyh212 <bdesm052@uottawa.ca>
2024-01-12 09:45:37 -0500

    Initial file

---
+++ changing_base.py
@@ -0,0 +1,35 @@
+def base_10_to_2(n):
+	h = int(n)
+	l = []
+	while h > 0:
+		l.append(h%2)
+		h = h//2
+	return l[::-1]
+
+p = base_10_to_2(24)
+
+
+
+def base_10_to_8(n):
+	h = int(n)
+	l = []
+	while h > 0:
+		l.append(h%8)
+		h = h//8
+	return l[::-1]
+
+p = base_10_to_8(73)
+
+
+
+def base_10_to_16(n):
+	h = int(n)
+	l = []
+	while h > 0:
+		l.append(h%16)
+		h = h//16
+
+
+	return l[::-1]
+
+p = base_10_to_16(73.4)
