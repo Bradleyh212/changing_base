e780e6dc5df739deb62e4c03bc3dd675d245e1c5
Bradleyh212 <bdesm052@uottawa.ca>
2024-01-22 13:32:16 -0500

    Adding decimal conversion and dot to seperate the whole number and deciaml

--- changing_base.py
+++ changing_base.py
@@ -7,7 +7,6 @@ def base_10_to_2(n):
 	if n > 0:
 		dec_n = n - whole_n
 
-
 	while whole_n > 0:
 		binary_int.append(whole_n % 2)
 		whole_n //= 2
@@ -19,21 +18,31 @@ def base_10_to_2(n):
 		binary_dec.append(int_n)
 		dec_n -= int_n
 
-	whole_binary = binary_int + binary_dec
+	whole_binary = binary_int + ["_"] + binary_dec
 
 	return whole_binary
 
-
-
-
 def base_10_to_8(n):
-	h = int(n)
-	l = []
-	while h > 0:
-		l.append(h%8)
-		h = h//8
-	return l[::-1]
+	whole_n = int(n)
+	octal_int = []
+	octal_dec = []
+	if n > 0:
+		dec_n = n - whole_n
 
+	while whole_n > 0:
+		octal_int.append(whole_n % 8)
+		whole_n //= 8
+	octal_int = octal_int[::-1]
+
+	while dec_n > 0:
+		dec_n *= 8
+		int_n = int(dec_n)	
+		octal_dec.append(int_n)
+		dec_n -= int_n
+
+	whole_octal = octal_int + ["_"] + octal_dec
+
+	return whole_octal
 
 
 def base_10_to_16(n):
@@ -45,6 +54,29 @@ def base_10_to_16(n):
 
 
 	return l[::-1]
+
+
+def base_10_to_16(n):
+	whole_n = int(n)
+	hexa_int = []
+	heax_dec = []
+	if n > 0:
+		dec_n = n - whole_n
+
+	while whole_n > 0:
+		hexa_int.append(whole_n % 16)
+		whole_n //= 16
+	hexa_int = hexa_int[::-1]
+
+	while dec_n > 0:
+		dec_n *= 16
+		int_n = int(dec_n)	
+		heax_dec.append(int_n)
+		dec_n -= int_n
+
+	whole_hexa = hexa_int + ["_"] + heax_dec
+
+	return whole_hexa
 
 
 
